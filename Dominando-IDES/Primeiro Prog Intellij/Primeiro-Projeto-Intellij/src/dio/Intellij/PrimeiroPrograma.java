e716e9ae7ba84e6b364c2168e68d05d21cc0717c
Thiago Ruan Costa <thiagorcosta.26@gmail.com>
2022-05-09 23:03:02 -0300

    Atualização Dominando IDE'S

---
+++ Dominando-IDES/Primeiro Prog Intellij/Primeiro-Projeto-Intellij/src/dio/Intellij/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package dio.Intellij;
+
+import model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World! " + (a+b));*/
+
+
+    }
+}
