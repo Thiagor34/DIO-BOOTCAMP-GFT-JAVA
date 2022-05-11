4dc3191b99943b9eaaf6aab669c0feda2ce62a35
Thiago Ruan Costa <thiagorcosta.26@gmail.com>
2022-05-11 08:54:02 -0300

    Atualizações Java fundamental

---
+++ Java Fundamental/Tipos-Dados/src/dio/gft/bootcamp/Main.java
@@ -0,0 +1,54 @@
+package dio.gft.bootcamp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short s2 = 40000;
+
+        //int i1 = =10000000000;
+        int i2 = 28500;
+
+        long l1 = 1000000000000000000l;
+        long l2 = 2004005000500055000l;
+
+        //float f1 = 4.5;
+        float f2 = 10.68f;
+
+        double d1 = 85.69;
+        double d2 = 99.84d;
+
+        char c1 = 'W';
+        //char c2 = 'Tw';
+        char c3 = '\u0057';
+
+        String st1 = "João";
+        String st2= "Thiago";
+        String st3 = "aaaaa  bbbbbb ccccccc 111111   %#$¨&&";
+        String dt1 = "11/05/2022";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(b1);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(bo1);
+        System.out.println(bo2);
+    }
+}
